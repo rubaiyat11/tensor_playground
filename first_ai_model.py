00548047709a16d3b7229a485b4180d2e25d937d
Weeeeeeeeeeee <rubaiyatabdullahrabi71@gmail.com>
2026-06-18 21:34:54 +0600

    First AI Model

--- first_ai_model.py
+++ first_ai_model.py
@@ -9,23 +9,85 @@ class LinearRegressionModel(nn.Module):
         self.weight = nn.Parameter(torch.randn(1))
         self.bias = nn.Parameter(torch.randn(1))
 
+
     
     def forward(self, x):
         return self.weight * x + self.bias
     
 
+
+x = torch.arange(0, 11).float()
+
+y = 2*x + 1
+
+print(x)
+print(y)
+
+train_x = x[:8]
+train_y = y[:8]
+
+test_x = x[8:]
+test_y = y[8:]
+
+print(train_x)
+print(train_y)
+
+print(test_x)
+print(test_y)
+
 model = LinearRegressionModel()
 
-print(model)
-print(model.weight)
-print(model.bias)
+loss_fn = nn.MSELoss()
 
-x = torch.tensor([1.0, 2.0, 3.0])
+optimizer = torch.optim.SGD(
+    model.parameters(),
+    lr=0.01
+)
 
-prediction = model(x)
 
-print(prediction)
-print(prediction.shape)
+epochs = 500
 
-print(list(model.parameters()))
+for epoch in range(epochs):
+    
+    predictions = model(train_x)
+
+    loss = loss_fn(
+        predictions,
+        train_y
+    )
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
+
+    if epoch % 10 == 0:
+        
+        print(f"Epch {epoch}")
+
+        print(f"Loss: {loss}")
+
+        print(model.state_dict())
+
+        print()
+
+print()
+
+print("Final parameters")
+
 print(model.state_dict())
+
+predictions = model(test_x)
+
+print()
+
+print("Test predictions")
+
+print(predictions)
+
+print()
+
+print("Actual values")
+
+print(test_y)
